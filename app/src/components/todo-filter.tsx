c0aa2b1c3c28df7842a5928d69a53a0098aeca3f
Arniu <arniu2006@gmail.com>
2021-12-30 21:18:38 +0800

    Move types outside

--- app/src/components/todo-filter.tsx
+++ app/src/components/todo-filter.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import useFilter from '../hooks/use-filter';
-import type { Filter } from 'todos-types';
+import type { Filter } from 'types';
 
 const FILTERS: Filter[] = ['all', 'active', 'completed'];
 
